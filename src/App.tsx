4ea0770fc081f5743ff5b51802f1665e852a7019
[dyad] <git@dyad.sh>
2026-04-14 11:45:37 +0530

    Reverted all changes back to version 91ea15e79087d5dc3fe04448e170147dca0c83b7

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Auth from "./pages/Auth";
-import TournamentDetail from "./pages/TournamentDetail";
-import LiveMatch from "./pages/LiveMatch";
-import ScoringPage from "./pages/ScoringPage";
-import LiveBroadcast from "./pages/LiveBroadcast";
-import CourtPage from "./pages/CourtPage";
-import BroadcastCenter from "./pages/BroadcastCenter";
 
 const queryClient = new QueryClient();
 
@@ -23,13 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/tournaments" element={<TournamentDetail />} />
-          <Route path="/live-match" element={<LiveMatch />} />
-          <Route path="/broadcast/:matchId" element={<LiveBroadcast />} />
-          <Route path="/scoring/:matchId" element={<ScoringPage />} />
-          <Route path="/court" element={<CourtPage />} />
-          <Route path="/broadcast/center" element={<BroadcastCenter />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
