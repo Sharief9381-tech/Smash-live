cacefab112098574195c6d5341638d4103a23397
[dyad] <git@dyad.sh>
2026-04-14 11:35:14 +0530

    [dyad] Connected Supabase for real-time cloud database and sequential Smash ID generation starting from 1. - wrote 4 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Auth from "./pages/Auth";
+import TournamentDetail from "./pages/TournamentDetail";
+import LiveMatch from "./pages/LiveMatch";
+import ScoringPage from "./pages/ScoringPage";
+import LiveBroadcast from "./pages/LiveBroadcast";
+import CourtPage from "./pages/CourtPage";
+import BroadcastCenter from "./pages/BroadcastCenter";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +23,13 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/tournaments" element={<TournamentDetail />} />
+          <Route path="/live-match" element={<LiveMatch />} />
+          <Route path="/broadcast/:matchId" element={<LiveBroadcast />} />
+          <Route path="/scoring/:matchId" element={<ScoringPage />} />
+          <Route path="/court" element={<CourtPage />} />
+          <Route path="/broadcast/center" element={<BroadcastCenter />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
