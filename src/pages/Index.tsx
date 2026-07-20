563bdb530ddfb5e700bd382fe36f563f5442f2a7
[dyad] <24B11IT094@adityauniversity.in>
2026-07-20 14:54:09 +0530

    [dyad] Fixing "cn is not defined" error - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,6 +13,7 @@ import {
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
 import { Link } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 
 const Index = () => {
   return (
