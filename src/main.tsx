ddbc8beea79f1fb023acc44241effbbcf8bfdaf4
[dyad] <24B11IT094@adityauniversity.in>
2026-07-20 18:38:34 +0530

    [dyad] Fixed preview and standardized project structure - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
